e500ee791f33f5e98c283bcf7bf4d2312b2b8daf
maungshine <shinekoko1276@gmail.com>
2024-05-13 12:41:15 +0630

    change content for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Link from "next/link";
 const certificates = [
   {
     id: '1',
-    title: "Web Development Traning And Intership With Php and Laravel",
+    title: "Web Development Traning And Internship With Php and Laravel",
     year: '20-Oct-2023 To 20-Mar-2024',
   },
   {
@@ -42,7 +42,8 @@ const languages = [
   'Sql',
   'MangoDB',
   'Css',
-  'Html'
+  'Html',
+  'Git'
 ]
 
 const frameworks = [
@@ -72,37 +73,38 @@ export default function Home() {
           <ReactLogo /> */}
           <ProfileImage />
           <p className="dark:text-default-500 indent-8 text-2xl my-8 text-center font-mono">
-            I&apos;m Maung Shine, a frontend and full-stack developer passionate about crafting immersive digital
-            experiences. With expertise in React and Next.js, I bring creativity and precision to every project I touch.
-            Let&apos;s build something amazing together.
+            I&apos;m Maung Shine, a frontend and full-stack developer passionate about crafting software that is useful, yet apealing in visual
+            using React and Nextjs.
           </p>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div className="grid grid-rows-2 col-span-1 w-full gap-4">
 
               <article className="flex flex-col bg-white/30 backdrop-blur-sm gap-4 dark:bg-[#080808] dark:text-default-500 border items-center dark:border-default-100 rounded-md p-4">
-                <h3 className="font-semibold text-center inline-block">My Educational Background</h3>
-                <p className="indent-4 text-sm">
-                  I sudied Maritme Technology at Myanmar Mercantile Marine College. As I did&apos;t have computer science background,
-                  I had to learn to code through online courses Like CS50, and local programming courses and web development training.
-                  Since I strongly believed in learning by getting my hands drity with code, I built many projects on my own. Please
-                  check them out at <Link href='/projects' className="hover:border-b-2 hover:border-secondary-500 text-primary-500">Projects</Link> page.
+                <h3 className="font-semibold text-center inline-block text-2xl font-serif">Working Experience</h3>
+
+
+                <p className="text-sm mt-8 self-start">Web Developer Training And Internship</p>
+                <p className="text-xs self-start">20-Oct-2023 To 20-Mar-2024</p>
+                <p className="text-sm">
+                  In this position, I primarily work on a project called, Win Bo Myint which is a accounting and business training school. The website includes frontend to showcase their school and courses and admin dashboard to handle the frontend. We used laravel and php for fullstack development. Other technologies include bootstarp for styling, filament for cms dashboard and some Javascript for interacitvity.
                 </p>
+
               </article>
               <article className="flex flex-col bg-white/30 backdrop-blur-sm gap-4 dark:bg-[#080808] dark:text-default-500 border items-center dark:border-default-100 rounded-md p-4">
-                <h3 className="font-semibold text-center inline-block">Areas to be explored in future</h3>
-                <p className="indent-4 text-sm">
+                <h3 className="font-semibold text-center inline-block text-2xl font-serif">Areas to be explored in future</h3>
+                <p className="mt-8 text-sm">
                   I&apos;m interested in learning and developing Generative AI application in the future in my spare time. And another skill that I&apos;m aming to pursue in the future is developing 3D models and animations.
                 </p>
               </article>
             </div>
             <article className="flex flex-col bg-white/30 backdrop-blur-sm gap-4 dark:bg-[#080808] dark:text-default-500 border dark:border-default-100 rounded-md p-4">
-              <h3 className="font-semibold text-center inline-block">Languages and Framework I can use</h3>
-              <div className="text-sm space-y-8">
+              <h3 className="font-semibold text-center inline-block text-2xl font-serif">Languages and Frameworks</h3>
+              <div className="text-sm space-y-8 mt-8">
                 <div className="space-y-2 text-sm border dark:border-default-200 p-4 rounded-md dark:bg-black/30 backdrop-blur-sm">
                   <h3 className="font-semibold text">Languages</h3>
                   <ul className="flex gap-2 shrink-0 flex-wrap">
                     {languages.map((lang) => {
-                      return <li key={lang} className="border text-xs border-cyan-400 rounded-md p-2">{lang}</li>
+                      return <li key={lang} className="border text-xs border-cyan-400 rounded-md p-2 font-mono">{lang}</li>
                     })}
                   </ul>
                 </div>
@@ -111,7 +113,7 @@ export default function Home() {
                   <h3 className="font-semibold">Frameworks and Libraries</h3>
                   <ul className="flex gap-2 shrink-0 flex-wrap">
                     {frameworks.map((lang) => {
-                      return <li key={lang} className="border text-xs border-orange-400 rounded-md p-2">{lang}</li>
+                      return <li key={lang} className="border text-xs border-orange-400 rounded-md p-2 font-mono">{lang}</li>
                     })}
                   </ul>
                 </div>
@@ -120,7 +122,7 @@ export default function Home() {
                   <h3 className="font-semibold">CMS</h3>
                   <ul className="flex gap-2 shrink-0 flex-wrap">
                     {cms.map((lang) => {
-                      return <li key={lang} className="border text-xs border-indigo-400 rounded-md p-2">{lang}</li>
+                      return <li key={lang} className="border text-xs border-indigo-400 rounded-md p-2 font-mono">{lang}</li>
                     })}
                   </ul>
                 </div>
@@ -128,7 +130,7 @@ export default function Home() {
               </div>
             </article>
             <article className="flex col-span-1 md:col-span-2 bg-white/30 backdrop-blur-sm flex-col gap-4 dark:bg-[#080808] dark:text-default-500 border items-center dark:border-default-100 rounded-md p-8">
-              <h3 className="font-semibold text-center inline-block">Diploma and Certificates that I achieved</h3>
+              <h3 className="font-semibold text-center inline-block text-2xl font-serif">Diploma and Certificates</h3>
               <div className="flex justify-start w-full">
                 <Timeline timelineElements={certificates} />
               </div>

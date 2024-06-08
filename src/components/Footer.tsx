ff03544e8a4a9b59a9540f0c198e281f088dddde
Maung Shine <shinekoko1276@gmail.com>
2024-06-09 01:24:03 +0630

    change linked in url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,54 @@
-import { Link } from "@nextui-org/react"
-import { AiFillFacebook, AiFillGithub, AiFillLinkedin, AiFillMail, AiFillRedEnvelope } from "react-icons/ai"
-
+import { Link } from "@nextui-org/react";
+import {
+  AiFillFacebook,
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillMail,
+  AiFillRedEnvelope,
+} from "react-icons/ai";
 
 const Footer = () => {
-    return (
-        <footer className="flex flex-col gap-4 items-center justify-center mt-auto py-8">
-            <div className="flex justify-center gap-8">
-                <Link href="/" className="text-default-500 border-b-2 hover:border-primary-400 border-default-400">
-                    Home
-                </Link>
-                <Link href="/projects" className="text-default-500 border-b-2 hover:border-primary-400 border-default-400">
-                    Projects
-                </Link>
-                <Link href="/contact" className="text-default-500 border-b-2 hover:border-primary-400 border-default-400">
-                    Contact
-                </Link>
-            </div>
-            <div className="flex justify-center gap-2">
-                <Link href="https://www.linkedin.com/in/maung-shine-5aa254a6" target="_balnk">
-                    <AiFillLinkedin className="text-2xl dark:text-default-500 text-default-800" />
-                </Link>
-                <Link href="https://github.com/maungshine" target="_blank">
-                    <AiFillGithub className="text-2xl dark:text-default-500 text-default-800" />
-                </Link>
-                <Link href="mailto: shinekoko1276@gmail.com">
-                    <AiFillMail className="text-2xl dark:text-default-500 text-default-800" />
-                </Link>
-            </div>
-            <div className="flex justify-center text-xs">
-                <p>Copyright &copy; {new Date().getFullYear()}. All rights reserved by Maung Shine</p>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="flex flex-col gap-4 items-center justify-center mt-auto py-8">
+      <div className="flex justify-center gap-8">
+        <Link
+          href="/"
+          className="text-default-500 border-b-2 hover:border-primary-400 border-default-400"
+        >
+          Home
+        </Link>
+        <Link
+          href="/projects"
+          className="text-default-500 border-b-2 hover:border-primary-400 border-default-400"
+        >
+          Projects
+        </Link>
+        <Link
+          href="/contact"
+          className="text-default-500 border-b-2 hover:border-primary-400 border-default-400"
+        >
+          Contact
+        </Link>
+      </div>
+      <div className="flex justify-center gap-2">
+        <Link href="https://www.linkedin.com/in/maungshine" target="_balnk">
+          <AiFillLinkedin className="text-2xl dark:text-default-500 text-default-800" />
+        </Link>
+        <Link href="https://github.com/maungshine" target="_blank">
+          <AiFillGithub className="text-2xl dark:text-default-500 text-default-800" />
+        </Link>
+        <Link href="mailto: shinekoko1276@gmail.com">
+          <AiFillMail className="text-2xl dark:text-default-500 text-default-800" />
+        </Link>
+      </div>
+      <div className="flex justify-center text-xs">
+        <p>
+          Copyright &copy; {new Date().getFullYear()}. All rights reserved by
+          Maung Shine
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,89 +1,101 @@
-'use client';
-import { NavbarContent, Navbar, NavbarMenuToggle, NavbarBrand, NavbarItem, Link, NavbarMenu, NavbarMenuItem } from "@nextui-org/react"
+"use client";
+import {
+  NavbarContent,
+  Navbar,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarItem,
+  Link,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/react";
 import { useState } from "react";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { usePathname } from "next/navigation";
 
-
 const menuItems = [
-    {
-        name: 'Home',
-        url: '/',
-    },
-    {
-        name: 'Projects',
-        url: '/projects',
-    },
-    {
-        name: 'Contact',
-        url: '/contact'
-    }
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Projects",
+    url: "/projects",
+  },
+  {
+    name: "Contact",
+    url: "/contact",
+  },
 ];
 
 const NavBar = () => {
-    const pathname = usePathname();
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    return (
-        <Navbar onMenuOpenChange={setIsMenuOpen} className="bg-white/40 backdrop-blur-sm dark:bg-black dark:backdrop-blur-none">
-            <NavbarContent>
-                <NavbarMenuToggle
-                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-                    className="sm:hidden"
-                />
-                <NavbarBrand>
-                    <Link href="/" className="font-semibold text-default-900 text-2xl">M/S</Link>
-                </NavbarBrand>
-            </NavbarContent>
+  const pathname = usePathname();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <Navbar
+      onMenuOpenChange={setIsMenuOpen}
+      className="bg-white/40 backdrop-blur-sm dark:bg-black dark:backdrop-blur-none"
+    >
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <Link href="/" className="font-semibold text-default-900 text-2xl">
+            M/S
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
 
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {menuItems.map((item) => (
+          <NavbarItem key={item.name}>
+            <Link
+              color="foreground"
+              href={item.url}
+              className={`${
+                pathname === item.url && "border-b-2 border-primary-500"
+              } hover:border-b hover:border-primary-500`}
+            >
+              {item.name}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <ThemeSwitcher />
+        </NavbarItem>
+        <NavbarItem className="flex items-center">
+          <Link href="https://www.linkedin.com/in/maungshine" target="_balnk">
+            <AiFillLinkedin className="text-2xl text-default-800 dark:text-default-500" />
+          </Link>
+        </NavbarItem>
+        <NavbarItem className="flex items-center">
+          <Link href="https://github.com/maungshine" target="_blank">
+            <AiFillGithub className="text-2xl dark:text-default-500 text-default-800" />
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
 
-                {menuItems.map((item) =>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color="foreground"
+              href={item.url}
+              size="lg"
+            >
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+};
 
-                    <NavbarItem key={item.name}>
-                        <Link color="foreground" href={item.url} className={`${pathname === item.url && 'border-b-2 border-primary-500'} hover:border-b hover:border-primary-500`}>
-                            {item.name}
-                        </Link>
-                    </NavbarItem>
-                )}
-
-            </NavbarContent>
-            <NavbarContent justify="end" >
-                <NavbarItem>
-                    <ThemeSwitcher />
-                </NavbarItem>
-                <NavbarItem className="flex items-center">
-
-                    <Link href="https://www.linkedin.com/in/maung-shine-5aa254a6" target="_balnk">
-                        <AiFillLinkedin className="text-2xl text-default-800 dark:text-default-500" />
-                    </Link>
-                </NavbarItem >
-                <NavbarItem className="flex items-center">
-                    <Link href="https://github.com/maungshine" target="_blank">
-                        <AiFillGithub className="text-2xl dark:text-default-500 text-default-800" />
-                    </Link>
-                </NavbarItem>
-
-            </NavbarContent>
-
-            <NavbarMenu>
-                {menuItems.map((item, index) => (
-                    <NavbarMenuItem key={`${item}-${index}`}>
-                        <Link
-
-                            className="w-full"
-                            color="foreground"
-                            href={item.url}
-                            size="lg"
-                        >
-                            {item.name}
-                        </Link>
-                    </NavbarMenuItem>
-                ))}
-            </NavbarMenu>
-        </Navbar>
-
-    )
-}
-
-export default NavBar
+export default NavBar;
